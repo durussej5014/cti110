17eb9dccfcbe1e377785f4b446bb5ade1af35bb2
durussej5014 <71241863+durussej5014@users.noreply.github.com>
2020-09-13 23:05:00 -0400

    Add files via upload
    First attempt at writing code in Python.  I do not believe I fully passed this assignment.

---
+++ P1HW2_BasicMath_DuRusselJayson.py
@@ -0,0 +1,21 @@
+print('This is my first attempt at working withing Python by writing some basic code')
+
+print('Date: 13SEP2020')
+
+print('CTI-110 P1HW2 - Basic Match')
+
+print('Jayson P DuRussel')
+
+print('Please Enter first number:', '5')
+
+print('Please Enter second number:', '10')
+
+print('First number entered: 5')
+
+print('Second number entered: 10')
+
+print('Sum of both numbers : 15')
+
+print('Result of Multiplying the two numbers: 50')
+
+
